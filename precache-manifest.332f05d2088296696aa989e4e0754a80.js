ce21c4c1a6216b2f62ebc7d1376f3432cd7ab81d
Will Parks <wparks08@gmail.com>
2020-02-16 15:46:50 -0800

    Updates

---
+++ precache-manifest.332f05d2088296696aa989e4e0754a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5531574d11ac395c2c861b8aab8a0de",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "681f0c6c4be2f95b8191",
+    "url": "/clicky-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "646a75c58be7259d17ba",
+    "url": "/clicky-game/static/js/2.8c5b1c2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.8c5b1c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681f0c6c4be2f95b8191",
+    "url": "/clicky-game/static/js/main.04306cdd.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
