b2174ff2c93368910eac4c0337ebecf3481afacd
Will Parks <wparks08@gmail.com>
2020-02-16 16:02:01 -0800

    attempt github pages image path fix

--- src/components/Gamepiece/index.js
+++ src/components/Gamepiece/index.js
@@ -3,10 +3,13 @@ import PropTypes from "prop-types";
 import path from "path";
 
 function Gamepiece(props) {
-    let imagePath = path.join(__dirname, props.image);
-
     return (
-        <img src={imagePath} className="img-fluid img-thumbnail" onClick={props.handleGamepieceClick} alt="Gamepiece" />
+        <img
+            src={"." + props.image}
+            className="img-fluid img-thumbnail"
+            onClick={props.handleGamepieceClick}
+            alt="Gamepiece"
+        />
     );
 }
 
